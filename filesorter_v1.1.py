853fc16af4343e8cfcae858b6f3ce6c4b0b63b8f
Alan Largaespada <alargaes@fiu.edu>
2023-07-18 18:50:03 -0400

    removed useless else

--- filesorter_v1.1.py
+++ filesorter_v1.1.py
@@ -10,25 +10,24 @@ while not os.path.exists(path):
         path = input('Please input the path of desired sorted directory.\n')
     elif (answer == 'N') | (answer == 'n'):
         exit()
-else:
-    os.chdir(path)
 
-    for files in os.listdir():
-        if os.path.isfile(os.getcwd() + '\\' + files):
-            name, extension = os.path.splitext(os.getcwd() + '\\' + files)
-            extension = extension.replace('.', '')
-        
-            #If dir of file type already exists -> moves file
-            if os.path.isdir(os.getcwd() + '\\' + extension):
-                shutil.move(os.getcwd() + '\\' + files, os.getcwd() + '\\' + extension)
-                print(name + ' has been moved to ' + extension + ' directory.')
-        
-            #Makes dir of specific file type -> moves file
-            else:
-                os.mkdir(os.getcwd() + '\\' + extension)
-                shutil.move(os.getcwd() + '\\' + files, os.getcwd() + '\\' + extension)
-                print(extension + ' directory has been created.')
-                print(name + ' has been moved to ' + extension + ' directory.')
+os.chdir(path)
+for files in os.listdir():
+    if os.path.isfile(os.getcwd() + '\\' + files):
+        name, extension = os.path.splitext(os.getcwd() + '\\' + files)
+        extension = extension.replace('.', '')
+    
+        #If dir of file type already exists -> moves file
+        if os.path.isdir(os.getcwd() + '\\' + extension):
+            shutil.move(os.getcwd() + '\\' + files, os.getcwd() + '\\' + extension)
+            print(name + ' has been moved to ' + extension + ' directory.')
+    
+        #Makes dir of specific file type -> moves file
+        else:
+            os.mkdir(os.getcwd() + '\\' + extension)
+            shutil.move(os.getcwd() + '\\' + files, os.getcwd() + '\\' + extension)
+            print(extension + ' directory has been created.')
+            print(name + ' has been moved to ' + extension + ' directory.')
 
 
 
